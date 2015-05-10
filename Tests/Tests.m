6cd641c9dc7f2dbee003ab95de77e16c43bdb56a
Oliver Letterer <oliver.letterer@gmail.com>
2015-05-10 17:51:20 +0200

    Adds tests

--- Tests/Tests.m
+++ Tests/Tests.m
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
+#import <Expecta/Expecta.h>
 #import <SPLPing/SPLPing.h>
 
 @interface PingTests : XCTestCase
@@ -19,11 +20,6 @@
 
 @implementation PingTests
 
-- (void)setUp
-{
-    [super setUp];
-}
-
 - (void)tearDown
 {
     [super tearDown];
@@ -32,9 +28,39 @@
     self.ping = nil;
 }
 
-- (void)testExample
+- (void)testPingToLocalhost
 {
-    XCTAssert(YES, @"Pass");
+    SPLPingConfiguration *config = [[SPLPingConfiguration alloc] initWithPingInterval:1.0];
+    self.ping = [[SPLPing alloc] initWithIPv4Address:@"127.0.0.1" configuration:config];
+
+    __block SPLPingResponse *response = nil;
+    [self.ping setObserver:^(SPLPing * __nonnull ping, SPLPingResponse * __nonnull _response) {
+        response = _response;
+    }];
+    [self.ping start];
+
+    expect(response).willNot.beNil();
+    expect(response.error).to.beNil();
+    expect(response.duration).to.beGreaterThan(0.0);
+}
+
+- (void)testPingToGoogle
+{
+    SPLPingConfiguration *config = [[SPLPingConfiguration alloc] initWithPingInterval:1.0];
+
+    __block SPLPingResponse *response = nil;
+    [SPLPing pingToHost:@"google.com" configuration:config completion:^(SPLPing * __nullable ping, NSError * __nullable error) {
+        self.ping = ping;
+
+        [self.ping setObserver:^(SPLPing * __nonnull ping, SPLPingResponse * __nonnull _response) {
+            response = _response;
+        }];
+        [self.ping start];
+    }];
+
+    expect(response).willNot.beNil();
+    expect(response.error).to.beNil();
+    expect(response.duration).to.beGreaterThan(0.0);
 }
 
 @end
